df230ae7cb4293c38995848b33549cad2aa98539
Gui-dev <adrianon10@gmail.com>
2022-09-20 02:57:20 -0300

    feat: add hitbox_area and helper function kill

--- scripts/enemies/slime.gd
+++ scripts/enemies/slime.gd
@@ -4,6 +4,7 @@ class_name Slime
 
 var player_ref = null
 var velocity: Vector2
+var can_die: bool = false
 onready var texture: Sprite = $texture
 onready var animation: AnimationPlayer = $animation
 export(int) var speed
@@ -40,7 +41,10 @@ func verify_direction() -> void:
 
 
 func animate() -> void:
-  if velocity != Vector2.ZERO:
+  if can_die:
+    animation.play('dead')
+    set_physics_process(false)
+  elif velocity != Vector2.ZERO:
     animation.play('walk')
   else:
     animation.play('idle')
@@ -54,3 +58,13 @@ func _on_detection_area_body_entered(body: Node) -> void:
 func _on_detection_area_body_exited(body: Node) -> void:
   if body.is_in_group('player'):
     player_ref = null
+
+
+func kill(area: Area2D) -> void:
+  if area.is_in_group('player_attack'):
+    can_die = true
+
+
+func _on_animation_finished(anim_name: String) -> void:
+  if anim_name == 'dead':
+    queue_free()
